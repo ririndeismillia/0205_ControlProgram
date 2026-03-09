ea797fd44a0b705685628520af7e7a99ffd44c56
ririndeismillia <ririndeismillia@gmail.com>
2026-03-09 23:17:50 +0700

    commit6

--- coding2.cpp
+++ coding2.cpp
@@ -14,4 +14,17 @@ string Status_lulus(float r){
 int main(){
     float nilBI,nilMT,rerata;
     string status;
+
+    cout <<"Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout <<"Masukkan nilai Matematika = ";
+    cin >> nilMT;
+
+     //rerata = (nilBI+nilMT)/2;
+    //if (rerata >= 60)
+    //    status = "lolos";
+    //else
+      // status = "Gagal";
+    rerata = Rata_rata(nilBI,nilMT);
+    //status = Status_lulus(rerata);
 }
